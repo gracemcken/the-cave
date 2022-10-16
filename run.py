8be83a2fa00c0d4d272438e6ffe75bc4d0ddb24e
gracemcken <grace.ellen.mckenna@gmail.com>
2022-10-16 22:15:31 +0100

    Add get_race func and add race to sheet func

--- run.py
+++ run.py
@@ -41,8 +41,41 @@ def add_name():
     worksheet_to_update.update_cell(2,1, name)
     
 
+def get_race():
+    """
+    Player chooses their race, each different option add different statistics to the player's core scores
+    """
+    fprint("Tell me, what race are you?\n", 1)
+    fprint("Human: Adaptable and ambitious, humans are the jack of all trades when it comes to races.", 1)
+    fprint("Elf: Known for their beauty and grace, elves excel at acrobatics and swiftness.", 1)
+    fprint("Dwarf: Solid and stout, dwarves are as stubborn as they are strong.\n", 1)
+    while True:
+        race = input("My race is: ")
+        races = ["Human", "Dwarf", "Elf"]
+        if race in races:
+            print(f"Nice to meet you, {race}. You are the first {race} seen here in a long time.")
+            break
+        else:
+            print("Please type one of the races listed and ensure there is a capital letter.")
+            continue
+    return race
+
+def add_race():
+    """
+    Adds player race to character worksheet
+    """
+    worksheet_to_update = SHEET.worksheet('character')
+    worksheet_to_update.update_cell(2,2, race)
+    
+
+
+
+
 
 name = get_name()
+race = get_race()
 add_name()
+add_race()
+
 
 
