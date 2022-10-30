36af88b4f40c4fe17d42b7c2e4d4c99754ad509d
gracemcken <grace.ellen.mckenna@gmail.com>
2022-10-30 19:58:05 +0000

    Add fail_one func to events.py

--- events.py
+++ events.py
@@ -65,7 +65,7 @@ def decision_one(answer1):
         if luck >= 15:
             stage_2()
         else:
-            fail_one()
+            fail_two()
     else:
         print("You close your eyes and fall asleep.\n")
         print("The game is over. You will never know what could have been.")
@@ -78,3 +78,40 @@ def decision_one(answer1):
             print("GAME OVER. THANK YOU FOR PLAYING.")
         else:
             print("Please type either yes/Yes or no/No")
+
+
+def fail_one():
+    """
+    Function that decides what happens
+    should the player fail the first choice.
+    """
+    print("Bang! You trip over something and land on your hands")
+    print("and knees. It's painful and although you can't see it,")
+    print("you can feel blood trickle down the palms of your hands.\n")
+    print("You lose 5 health points.\n")
+    print("Do you...?")
+    print("1. Stand up and continue towards the light.\n")
+    print("2. Feel around the area for any item that could be of use to you.\n ")
+    print("3. Close your eyes again and wish for this game to end.\n")
+    while True:
+        answer1 = input("")
+        answers = ["1", "2", "3"]
+        if answer1 in answers:
+            print(
+                f"You have chosen option {answer1}."
+                " Let us see how you fare this time..."
+            )
+            decision_one(answer1)
+        else:
+            print("Please type either '1', '2', or '3'.")
+            continue
+        return answer1
+
+
+def stage_2():
+    """
+    Next stage of the story.
+    """
+    print("The source of the light is a iron lantern fixed to the wall.")
+    print("The cave seems to be starting to form into a carved out hallway,")
+    print("with more lanterns lining the pathway down.")
